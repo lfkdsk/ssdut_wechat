277f24689412fcda8b5d92ff5f9c018291a619df
lirz <lirunzhi99@hotmail.com>
2016-03-30 15:40:51 +0800

    取消md5

--- static/js/login.js
+++ static/js/login.js
@@ -2,5 +2,15 @@
  * Created by li-rz on 16-3-30.
  */
 $(document).ready(function () {
-    
+    $('form').on('submit', function (event) {
+        event.preventDefault();
+        
+
+
+
+        // $.ajax({
+        //     url: '/admin/login'
+        //
+        // })
+    });
 });
