358e02ff1f5680c93e28afb83756b1d545f4de24
lirz <lirunzhi99@hotmail.com>
2016-04-01 17:12:53 +0800

    修复token问题

--- static/js/login.js
+++ static/js/login.js
@@ -74,30 +74,30 @@ $(document).ready(function () {
         }
 
 
-        var token,
-            confirm_token = {username: value['username']},
+        var confirm_token = {username: value['username']},
             host = window.location.host;
         $.post('/admin/tokenget', JSON.stringify(confirm_token), function (data) {
-            token = data;
-        });
-        value['token'] = token;
-        value.password = md5(value.password);
-        $.ajax({
-            url: '/admin/jump',
-            type: 'POST',
-            data: JSON.stringify(value),
-            success: function (response) {
-                if (parseInt(response)) {
-                    window.location.replace("http://" + host + '/admin/index');
-                    return;
+            value.token = data;
+            value.password = md5(value.password);
+            $.ajax({
+                url: '/admin/jump',
+                type: 'POST',
+                data: JSON.stringify(value),
+                success: function (response) {
+                    if (parseInt(response)) {
+                        window.location.replace("http://" + host + '/admin/index');
+                        return;
+                    }
+                    warning();
+                    container.removeDisable();
+                },
+                error: function () {
+                    warning();
+                    container.removeDisable();
                 }
-                warning();
-                container.removeDisable();
-            },
-            error: function () {
-                warning();
-                container.removeDisable();
-            }
+            });
         });
+
+
     });
 });
