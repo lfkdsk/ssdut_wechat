e03f52141cf2a9ce20f0432d07c021ace1da3ba1
lirz <lirunzhi99@hotmail.com>
2016-04-08 20:33:34 +0800

    test JSON

--- static/js/admin.js
+++ static/js/admin.js
@@ -6,9 +6,36 @@ $(document).ready(function () {
         textarea: $('#editor')
     });
 
-    var page_type = 'about_college';
+    var page_type = $('ul.nav-sidebar li.active').find('a').get(0).dataset.goTo,
+        $element = {
 
-    $.post('/gethistory', page_type, function (response) {
-        console.log(response);
-    })
+        },
+        node,
+        element_data,
+        data;
+    console.log(data[0]);
+    $.post('/gethistory', {label: page_type}, function (response) {
+
+
+        for (var i = 0; i < data.length; ++i) {
+            element_data = JSON.parse(data[i]);
+            node += document.createElement('li');
+            console.log(node, element_data);
+        }
+        historyCallback();
+    });
+
+
+
+    function historyCallback () {
+        var $editor = $('#editor-article');
+
+        $element.editor =
+            $editor.find('li');
+
+        $element.history_list =
+            $editor.find('button[data-to-do="delete"]');
+
+
+    }
 });
